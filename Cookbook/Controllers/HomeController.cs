e8d6b65551e7acb0d6a0fcd1be5e7b0cc1810277
jack <jssgh6@mst.edu>
2018-08-20 07:34:24 -0500

    created List<Recipe> private prop as data service to all views added AddRecipe and SubmitRecipe actions

--- Cookbook/Controllers/HomeController.cs
+++ Cookbook/Controllers/HomeController.cs
@@ -1,26 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Cookbook.Models;
+using System.Linq;
 
 namespace Cookbook.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Recipe> RecipeService;
+
+        public HomeController()
+        {
+            RecipeService = GetRecipes();
+        }
+
         public IActionResult Index()
         {
             // todo get all recipes and pass to the view
+            return View(RecipeService);
+        }
+
+        public IActionResult AddRecipe() { return View(); }
+
+        public IActionResult SubmitRecipe(Recipe recipe)
+        {
+            if (!ModelState.IsValid) return RedirectToAction("Index");
+            var successful = true; // todo add item through recipe service
+            if (!successful) return BadRequest("Could not add item.");
+            return RedirectToAction("Index");
+        }
+
+        private List<Recipe> GetRecipes()
+        {
             var recipes = new List<Recipe>();
-            return View(recipes);
+
+            var recipe = new Recipe();
+            recipe.ID = 0;
+            recipe.Author = "Jack Savage";
+            recipe.DateAdded = DateTime.Now;
+            recipe.Title = "Hamburger";
+            recipe.Description = "This will hit the freakin spot. I promise you.";
+            recipe.Tags = new List<string>(new[] { "american", "burgers", "grilling" });
+            recipe.Directions = new List<Direction>(new[] {
+                new Direction() { Order = 1, Content = "cook the meat" },
+                new Direction() { Order = 2, Content = "build your sammy" },
+                new Direction() { Order = 3, Content = "eat it" },
+            });
+            recipe.Ingredients = new List<Ingredient>(new[] {
+                new Ingredient() { Quantity = "1 pound", Name = "ground beef" },
+                new Ingredient() { Quantity = "1", Name = "wheat bun" },
+                new Ingredient() { Quantity = "1 head", Name = "lettuce" },
+                new Ingredient() { Quantity = "1 slice", Name = "cheese" }
+            });
+
+            recipes.Add(recipe);
+
+            return recipes;
         }
 
         public IActionResult Recipe(int id)
         {
-            // todo get one recipe and pass it to view
-            return View();
+            var recipe = RecipeService.First(r => r.ID == id);
+            return View(recipe);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
